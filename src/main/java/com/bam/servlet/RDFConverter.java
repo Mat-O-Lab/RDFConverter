e4c843daf0a7e81faf173ec7a506db882b0645cb
Andre Valdestilhas <firmao@gmail.com>
2021-11-22 15:47:16 +0100

    Starting

---
+++ src/main/java/com/bam/servlet/RDFConverter.java
@@ -0,0 +1,92 @@
+package com.bam.servlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RDFConverter
+ */
+@WebServlet("/RDFConverter")
+public class RDFConverter extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Default constructor.
+	 */
+	public RDFConverter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	static class Conf {
+		String yarrrml;
+		String chowlk;
+		String shacl;
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		if (request.getParameter("jsonfile") != null) {
+			String jsonFile = request.getParameter("jsonfile");
+			response.getWriter().append("Validating and uploading files to the triple store...");
+			try {
+				//String json = readUrl("http://www.javascriptkit.com/" + "dhtmltutors/javascriptkit.json");
+				String jsonConf = readUrl(jsonFile);
+				Gson gson = new Gson();
+				Conf conf = gson.fromJson(jsonConf, Conf.class);
+
+				response.getWriter().append(conf.yarrrml);
+				response.getWriter().append(conf.chowlk);
+				response.getWriter().append(conf.shacl);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			
+		}
+	}
+
+	private static String readUrl(String urlString) throws Exception {
+		BufferedReader reader = null;
+		try {
+			URL url = new URL(urlString);
+			reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			StringBuffer buffer = new StringBuffer();
+			int read;
+			char[] chars = new char[1024];
+			while ((read = reader.read(chars)) != -1)
+				buffer.append(chars, 0, read);
+
+			return buffer.toString();
+		} finally {
+			if (reader != null)
+				reader.close();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
