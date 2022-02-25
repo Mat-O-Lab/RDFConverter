86354884c23f6eda4c2f304eabf4007d03ed41f5
soudehMasoudian <soudeh.masoudian@gmail.com>
2022-02-25 14:37:33 +0100

    resolve dependency

--- YARRRML2RML/yarrrml2rml.py
+++ YARRRML2RML/yarrrml2rml.py
@@ -1,10 +1,6 @@
 # dependencies related to rml conversion
 
-import constants as constants
-import mapping as mapping_mod
-import source as source_mod
-import subject as subject_mod
-import predicateobject as predicate_object_mod
+import pretty_yarrrml2rml as yarrrml2rml
 import yaml
 
 # dependencies related to api
@@ -13,23 +9,28 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
+# we used pretty-yarrrml2rml lib, but since it has some errors
+# in validation part, so we overridden this method to resolve
+# the problem
+
 @app.route('/rmlconversion', methods=['POST'])
 def translate():
     print("------------------------START TRANSLATING YARRRML TO RML-------------------------------")
 
     yarrrml_data = yaml.safe_load(request.values['test'])
 
-    list_initial_sources = source_mod.get_initial_sources(yarrrml_data)
-    rml_mapping = [mapping_mod.add_prefix(yarrrml_data)]
+    list_initial_sources = yarrrml2rml.source_mod.get_initial_sources(yarrrml_data)
+    rml_mapping = [yarrrml2rml.mapping_mod.add_prefix(yarrrml_data)]
     try:
-        for mapping in yarrrml_data.get(constants.YARRRML_MAPPINGS):
-            subject_list = subject_mod.add_subject(yarrrml_data, mapping)
-            source_list = source_mod.add_source(yarrrml_data, mapping, list_initial_sources)
-            pred = predicate_object_mod.add_predicate_object_maps(yarrrml_data, mapping)
+        for mapping in yarrrml_data.get(yarrrml2rml.constants.YARRRML_MAPPINGS):
+            subject_list = yarrrml2rml.subject_mod.add_subject(yarrrml_data, mapping)
+            source_list = yarrrml2rml.source_mod.add_source(yarrrml_data, mapping, list_initial_sources)
+            pred = yarrrml2rml.predicate_object_mod.add_predicate_object_maps(yarrrml_data, mapping)
             it = 0
             for source in source_list:
                 for subject in subject_list:
-                    map_aux = mapping_mod.add_mapping(mapping, it)
+                    map_aux = yarrrml2rml.mapping_mod.add_mapping(mapping, it)
                     if type(source) is list:
                         rml_mapping.append(map_aux + source[0] + subject + pred + source[1])
                     else:
