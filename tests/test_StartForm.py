e085fc9543c8f3fca296470bd1a0de8e25cb0de8
Horst Fellenberg <40174618+horstf@users.noreply.github.com>
2022-03-29 15:25:20 +0200

    add test files

--- tests/test_StartForm.py
+++ tests/test_StartForm.py
@@ -1,25 +1,44 @@
 import unittest
 import requests
 import yaml
+import rdflib
+from rdflib.compare import to_isomorphic, graph_diff
+
+def generate_joined_data() -> str:
+    pass
+
+with open('./resources/config.yml', 'r') as f:
+    CFG = yaml.safe_load(f)
+
+ENDPOINT = CFG['rdfconverter']['unittest']['location']['host'] + ':' + CFG['rdfconverter']['unittest']['location']['port'] + '/'
 
 class test_StartForm(unittest.TestCase):
     def test_translate(self):
-        with open("./resources/config.yml", "r") as cfg:
-            cfg = yaml.safe_load(cfg)
-
-        yarrrml = open(cfg['rdfconverter']['unittest']['yarrrml2rml']['data']['input']).read()
+        with open(CFG['rdfconverter']['unittest']['yarrrml2rml']['data']['input'], 'r') as f:
+            yarrrml = f.read()
         payload = {'yarrrml': yarrrml}
-        rml_output = requests.post(cfg['rdfconverter']['unittest']['yarrrml2rml']['address']['host'] + ':' +
-        cfg['rdfconverter']['unittest']['yarrrml2rml']['address']['port'] + '/' +
-        cfg['rdfconverter']['unittest']['yarrrml2rml']['address']['contextroot'], payload)
+        rml_output = requests.post(ENDPOINT + CFG['rdfconverter']['unittest']['yarrrml2rml']['contextroot'], payload)
 
         try:
-            expected_output = open(cfg['rdfconverter']['unittest']['yarrrml2rml']['data']['expectedOutput']).read()            
+            with open(CFG['rdfconverter']['unittest']['yarrrml2rml']['data']['expectedOutput'], 'r') as f:
+                expected_output = f.read()            
             self.assertEqual(expected_output, rml_output.text)
             
         except Exception as e:
             print(str(e))
             self.assertRaises(e, msg=e.__cause__)
+
+    def test_joindata_works(self):
+        with open(CFG['rdfconverter']['unittest']['joindata']['data']['test1']['input'], 'r') as f:
+            rml_input = f.read()
+        payload = {'rml_url': rml}
+        res = requests.post(ENDPOINT + CFG['rdfconverter']['unittest']['joindata']['contextroot'])
+
+
+
+        
+
+    
 
 if __name__ == '__main__':
     unittest.main()
