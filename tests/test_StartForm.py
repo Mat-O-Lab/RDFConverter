2b3606babda463ff9ae42e210c4255bf02816435
Horst Fellenberg <40174618+horstf@users.noreply.github.com>
2022-03-29 16:34:36 +0200

    add joindata test

--- tests/test_StartForm.py
+++ tests/test_StartForm.py
@@ -1,11 +1,9 @@
+import imp
 import unittest
 import requests
 import yaml
-import rdflib
+from rdflib import Graph
 from rdflib.compare import to_isomorphic, graph_diff
-
-def generate_joined_data() -> str:
-    pass
 
 with open('./resources/config.yml', 'r') as f:
     CFG = yaml.safe_load(f)
@@ -28,11 +26,52 @@ class test_StartForm(unittest.TestCase):
             print(str(e))
             self.assertRaises(e, msg=e.__cause__)
 
-    def test_joindata_works(self):
-        with open(CFG['rdfconverter']['unittest']['joindata']['data']['test1']['input'], 'r') as f:
-            rml_input = f.read()
-        payload = {'rml_url': rml}
+    def test_joindata(self):
+        payload = {'rml_url': CFG['rdfconverter']['unittest']['joindata']['data']['test1']['input']}
         res = requests.post(ENDPOINT + CFG['rdfconverter']['unittest']['joindata']['contextroot'])
+        res_graph = Graph()
+        res_graph.parse(data=res.text, format='ttl')
+
+        expectation = requests.get(CFG['rdfconverter']['unittest']['joindata']['data']['test1']['expectedOutput'])
+        expect_graph = Graph()
+        expect_graph.parse(data=expectation.text, format='ttl')
+
+        # canonicalize both graphs
+        iso1 = to_isomorphic(res_graph)
+        iso2 = to_isomorphic(expect_graph)
+
+        _, in_first, in_second = graph_diff(iso1, iso2)
+
+        # all nodes of the second graph are included in both graphs
+        self.assertEqual(len(in_second), 0)
+
+    def test_joindata_with_data_url(self):
+        payload = {
+            'rml_url': CFG['rdfconverter']['unittest']['joindata']['data']['test2']['input1'],
+            'data_url': CFG['rdfconverter']['unittest']['joindata']['data']['test2']['input2']
+        }
+        res = requests.post(ENDPOINT + CFG['rdfconverter']['unittest']['joindata']['contextroot'])
+        res_graph = Graph()
+        res_graph.parse(data=res.text, format='ttl')
+
+        expectation = requests.get(CFG['rdfconverter']['unittest']['joindata']['data']['test2']['expectedOutput'])
+        expect_graph = Graph()
+        expect_graph.parse(data=expectation.text, format='ttl')
+
+        # canonicalize both graphs
+        iso1 = to_isomorphic(res_graph)
+        iso2 = to_isomorphic(expect_graph)
+
+        _, in_first, in_second = graph_diff(iso1, iso2)
+
+        # all nodes of the second graph are included in both graphs
+        self.assertEqual(len(in_second), 0)
+
+    def validate_conforms(self):
+        pass
+
+    def validate_does_not_conform(self):
+        pass
 
 
 
