9281dc4ebe12652a7840ff76edbf9a0993ad0b19
Thomas Hanke <hanke@docker-dev.iwm.fraunhofer.de>
2023-09-05 13:46:36 +0200

    increased version number

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Setting(BaseSettings):
     app_name: str = "RDFConverter"
     admin_email: str = os.environ.get("ADMIN_MAIL") or "rdfconverter@matolab.org"
     items_per_user: int = 50
-    version: str = "v1.0.3"
+    version: str = "v1.0.4"
     config_name: str = os.environ.get("APP_MODE") or "development"
     openapi_url: str = "/api/openapi.json"
     docs_url: str = "/api/docs"
