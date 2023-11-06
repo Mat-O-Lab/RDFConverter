51f5bdae5390e9082bf7a32c20fce96134fe8de1
Thomas Hanke <38633260+ThHanke@users.noreply.github.com>
2023-11-06 09:19:25 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Setting(BaseSettings):
     contact_name: str = str(os.environ.get("ADMIN_NAME","Max Mustermann"))
     admin_email: str = str(os.environ.get("ADMIN_MAIL","app_admin@example.com"))
     items_per_user: int = 50
-    version: str = str(os.environ.get("APP_VERSION","v1.0.6"))
+    version: str = str(os.environ.get("APP_VERSION","v1.1.0"))
     config_name: str = str(os.environ.get("APP_MODE","development"))
     openapi_url: str ="/api/openapi.json"
     docs_url: str = "/api/docs"
