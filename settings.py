ab7ecf9b17d3348db4e32a997e7db8e7161a8d51
Thomas Hanke <hanke@docker-dev.iwm.fraunhofer.de>
2023-11-22 11:04:06 +0100

    updated settings

--- settings.py
+++ settings.py
@@ -3,14 +3,14 @@ from pydantic_settings import BaseSettings
 
 
 class Setting(BaseSettings):
-    name: str = str(os.environ.get("APP_NAME","BaseFastApiApp"))
-    contact_name: str = str(os.environ.get("ADMIN_NAME","Max Mustermann"))
-    admin_email: str = str(os.environ.get("ADMIN_MAIL","app_admin@example.com"))
+    name: str = str(os.environ.get("APP_NAME","RDFConverter"))
+    contact_name: str = str(os.environ.get("ADMIN_NAME","Thomas Hanke"))
+    admin_email: str = str(os.environ.get("ADMIN_MAIL","thomas.hanke@iwm.fraunhofer.de"))
     items_per_user: int = 50
-    version: str = str(os.environ.get("APP_VERSION","v1.1.1"))
+    version: str = str(os.environ.get("APP_VERSION","v1.1.2"))
     config_name: str = str(os.environ.get("APP_MODE","development"))
     openapi_url: str ="/api/openapi.json"
     docs_url: str = "/api/docs"
-    source: str = str(os.environ.get("APP_SOURCE","https://example.com"))
+    source: str = str(os.environ.get("APP_SOURCE","https://github.com/Mat-O-Lab/RDFConverter"))
     desc: str = "A service for joining and converting meta data documents based on YARRRML mapping files to RDF, optionally a validiation can be conducted using SHACL Shapes."
     org_site: str = "https://mat-o-lab.github.io/OrgSite"
