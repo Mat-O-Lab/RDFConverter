4b8c44616da9e4b0b1a5dd75a943366751611733
Thomas Hanke <hanke@docker-dev.iwm.fraunhofer.de>
2023-11-27 12:42:09 +0100

    updated version number

--- settings.py
+++ settings.py
@@ -7,7 +7,7 @@ class Setting(BaseSettings):
     contact_name: str = str(os.environ.get("ADMIN_NAME","Thomas Hanke"))
     admin_email: str = str(os.environ.get("ADMIN_MAIL","thomas.hanke@iwm.fraunhofer.de"))
     items_per_user: int = 50
-    version: str = str(os.environ.get("APP_VERSION","v1.1.2"))
+    version: str = str(os.environ.get("APP_VERSION","v1.1.3"))
     config_name: str = str(os.environ.get("APP_MODE","development"))
     openapi_url: str ="/api/openapi.json"
     docs_url: str = "/api/docs"
